7b57dff81c65b0bcdefd1f790ec1b4848eb69087
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-29 12:44:35 +0200

    first commit

---
+++ src/app/products/pages/product/product.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-product-page',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent {}
